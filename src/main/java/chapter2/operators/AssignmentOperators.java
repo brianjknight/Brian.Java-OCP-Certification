128d8bc97f85d91ddb20fac3bd2acafccdb6a7b5
Brian Knight <brian.knight@siliconmtn.com>
2023-11-21 14:12:52 -0700

    more updates

---
+++ src/main/java/chapter2/operators/AssignmentOperators.java
@@ -0,0 +1,21 @@
+package chapter2.operators;
+
+public class AssignmentOperators {
+
+	public static void main(String[] args) {
+		byte num = 6;
+		byte two = num=2;
+		long twotwo = (num=22);
+		
+		long other = twotwo=11;
+		
+		
+		System.out.println("num:" + num);
+		System.out.println("two:"+two);
+		System.out.println("twotwo:"+twotwo);
+		System.out.println("other:"+other);
+		
+		
+		
+	}
+}
