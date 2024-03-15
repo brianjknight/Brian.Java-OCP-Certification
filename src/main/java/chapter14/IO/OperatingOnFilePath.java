f6feb8ab96b31f907527d48dc0b4a143844da3a7
Brian Knight <brian.knight@siliconmtn.com>
2024-03-15 15:51:28 -0600

    Ch 14 progress

--- src/main/java/chapter14/IO/OperatingOnFilePath.java
+++ src/main/java/chapter14/IO/OperatingOnFilePath.java
@@ -8,12 +8,38 @@ import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class OperatingOnFilePath {
-
 	
 		public static void main(String[] args) throws IOException {
-			io();
+			// When you compile and run the Java program, 
+			// it executes from the project's root directory unless you specify a different directory explicitly.
+			// SO this main method is executing from Brian.Java-OCP-Certification NOT chapter14.IO.OperatingOnFilePath
+			// this is why the relative path is not from IO
+			// e.g. "../../../resources/chapter14/zoo.txt"
+			// "./" and "../main" are redundant 
+			Path zooTxtFile = Path.of("./src/main/../main/resources/chapter14/zoo.txt");
+			System.out.println("print Path object: " + zooTxtFile);
+			System.out.println("getParent(): " + zooTxtFile.getParent() );
+			System.out.println("absolute path: " + zooTxtFile.toAbsolutePath());
+			System.out.println("getFileName(): " + zooTxtFile.getFileName());
+			System.out.println("getRoot(): " + zooTxtFile.getRoot());
+			System.out.println("getRoot() of absolute path: " + zooTxtFile.toAbsolutePath().getRoot());
+			System.out.println("file exists: " + Files.exists(zooTxtFile));
 			
-			System.out.println("\nnio:");
+			System.out.println("normalize(): " + zooTxtFile.normalize());
+			System.out.println();
+			
+			System.out.println("view path: ");
+			for (int i=0; i<zooTxtFile.getNameCount(); i++) {
+				System.out.println("\t index: " + i + " index name:" + zooTxtFile.getName(i)); // notice last element is zoo.txt 
+			}
+			System.out.println("subpath(2,5): " + zooTxtFile.subpath(2, 5));
+			System.out.println();
+			
+			System.out.println("io():");
+			io();
+			System.out.println();
+			
+			System.out.println("nio:");
 			nio();
 		}
 		
@@ -51,7 +77,9 @@ public class OperatingOnFilePath {
 						System.out.println("subfile: " + subfile.getName());
 					} 
 				} 
-			} 
+			} else {
+				System.out.println("doesn't exist: " + directory);
+			}
 		}
 		
 		public static void nio() throws IOException {
@@ -69,7 +97,9 @@ public class OperatingOnFilePath {
 						stream.forEach(p -> System.out.println(" " + p.getFileName()));
 					} 
 				}
-			} 
+			}
+			
+			
 		}
 		
 }
